de197365ced2a75c752b42701996660b473f5b8c
ChristianMazzaro <131186715+ChristianMazzaro@users.noreply.github.com>
2023-07-20 12:29:51 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script >
+  export default {
+    components:{
+
+    },
+    data(){
+      return{
+
+      }
+    },
+    methods:{
+
+    }
+  }
+</script>
+
+<template>
+
+    <header>  
+      header
+    </header>
+
+    <main>
+      main
+    </main>
+
+    <footer>
+      footer
+    </footer>
+
+</template>
+
+<style lang="scss">
+  @use "assets/scss/main.scss" as *;
+
+</style>
